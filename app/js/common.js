727e505439a8715fafe99b1e4561d71b0f058969
a204566 <543635627@qq.com>
2016-03-28 10:25:12 +0800

    app

---
+++ app/js/common.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+//index
+//------------------------------------------------
+
+
+//侧栏
+//$("#cover").hide()
+//			var menuRight = document.getElementById( 'cbp-spmenu-s2' ),
+//				showRight = document.getElementById( 'nav-cell' ),
+//				body = document.body;
+//
+//		
+//			showRight.onclick = function() {
+//				$("#cover").toggle()
+//				classie.toggle( this, 'active' );
+//				classie.toggle( menuRight, 'cbp-spmenu-open' );
+//				disableOther( 'showRight' );
+//				$(body).css({"overflow-y":"hidden"})
+//			};
+//			
+//			
+////黑屏
+//			$("#cover").click(function(){
+//				$("#cover").toggle()
+//				classie.toggle( this, 'active' );
+//				classie.toggle( menuRight, 'cbp-spmenu-open' );
+//				disableOther( 'showRight' );
+//				})
+//$("#cbp-spmenu-s2 a").on("tap",function(){
+//		$("#cbp-spmenu-s2 a").removeClass("click")
+//		$(this).addClass("click")
+//		})
